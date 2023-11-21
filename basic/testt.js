d350c318f346f438388c0d28c6a808f2b132ee0f
Shashikant jha <130702921+Shashikant-75555@users.noreply.github.com>
2023-11-21 21:45:50 +0000

    config done

---
+++ basic/testt.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+console.log("shashi")
+
+const accountId = 14456
+let accountEmail ="shashi@google.com"
+var accountPascode = 123456
+accountCity = "varanasi"
+console.log(accountId);
+
+console.table()
