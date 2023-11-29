ebbb4cfd67f5f12cf4ff5362be8203188bfcd9dd
Shashikant jha <130702921+Shashikant-75555@users.noreply.github.com>
2023-11-29 12:05:41 +0000

    operation_done

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -33,3 +33,41 @@ let someNumber = 33
 let stringNumber= String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//*************************** Operations ************************************ */
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "hanuman
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+ 2;)
+console.log(1+ "2");
+console.log("1"+ 2+2);
+console.log(1+ 2+"2");
+
+console.log( 3+4*5%3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//link for study  https://tc39.es/ecma262/#sec-abstract-operations
