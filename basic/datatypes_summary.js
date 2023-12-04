0b2c9e64c742c1a6cd3e31cbec146b2ea724c97b
Shashikant jha <130702921+Shashikant-75555@users.noreply.github.com>
2023-12-04 18:36:38 +0000

    datatypes_summary added

---
+++ basic/datatypes_summary.js
@@ -0,0 +1,14 @@
+/* Primitive datatype
+
+ 7 types : String , Number , Boolearn , null , undefined , Symbol , BigInt
+
+*/
+
+const score
+
+
+  Non-Primitive datatype (Reference)
+
+  Array , Objects , Functions
+
+ */
