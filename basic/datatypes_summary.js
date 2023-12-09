e7fcf48c2088c5a7923bef80bff81b23f8659157
Shashikant jha <130702921+Shashikant-75555@users.noreply.github.com>
2023-12-09 08:36:04 +0000

    all set

--- basic/datatypes_summary.js
+++ basic/datatypes_summary.js
@@ -4,7 +4,24 @@
 
 */
 
-const score
+const score = 100
+const scoreValue = 100.3
+
+const ioutsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+console.log(id)
+console.log(anotherId)
+
+
+
+//const bigNumber = 3827687837223764n
+
+
 
 /*
   Non-Primitive datatype (Reference)
@@ -12,3 +29,10 @@ const score
   Array , Objects , Functions
 
  */
+
+  const heros = [ " shaktiman","naagraj","doga"];
+  {
+     name : "shashi"
+     //age : 22;
+     
+  }
