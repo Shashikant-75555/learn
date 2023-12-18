9339cb696057de567542e3fcaea53ac4cd317bf9
Shashikant jha <130702921+Shashikant-75555@users.noreply.github.com>
2023-12-18 15:05:22 +0000

    06done

--- basic/07_datesinJs.js
+++ basic/07_datesinJs.js
@@ -2,3 +2,13 @@
 
 let myDate = new Date()
 console.log(myDate.toString());
+// console.log(myDate.toDateString(),"toDateString");
+// console.log(myDate.toJSON(),"toJSO");
+// console.log(myDate.toLocaleString(),"toLocaleString")
+// console.log(myDate.toISOString(),"toISOString")
+// console.log(myDate.toLocaleDateString(),"toLocaleDateString")
+// console.log(myDate.toLocaleTimeString(),"toLocaleTimeString")
+
+
+
+console.log("sucess") 
