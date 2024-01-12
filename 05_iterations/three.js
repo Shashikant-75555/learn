117fa55153f50647f328354e9f865265599d6df3
Shashikant jha <130702921+Shashikant-75555@users.noreply.github.com>
2024-01-12 17:52:13 +0000

    map iteration done

---
+++ 05_iterations/three.js
@@ -0,0 +1,30 @@
+// for of
+
+
+// [{}, {},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+   
+}   
+
+const greetings = "hello world"
+for (const greet of greetings) {
+    if (greet==" ")
+    {
+    console.log("thik s likho bsdk")
+    break;
+    }
+     console.log(`Each char is ${greet}`)
+    
+}
+// const arr = [1,2,3,4,5,6]
+
+// console.log(`arr`,"  .  ")
+
+// Maps
+
+const map = new Map()
+
